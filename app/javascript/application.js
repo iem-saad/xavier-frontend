975bf80f35944f618e3de9091ce7fa5ae4cebf3a
Saad Abdullah <f190108@cfd.nu.edu.pk>
2022-12-27 18:57:45 +0500

    feat: UI tweaks and theme settlement

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,13 @@
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = false
 import "./controllers"
+// import "./custom/template.js"
+// import "./vendor/aos/aos.css"
+// import "./vendor/aos/aos.js"
+// import "./vendor/swiper/swiper-bundle.min.css"
+// import "./vendor/swiper/swiper-bundle.min.js"
+// import "./vendor/bootstrap/css/bootstrap.min.css"
+// import "./vendor/bootstrap-icons/bootstrap-icons.css"
+// import "./vendor/boxicons/css/boxicons.min.css"
+// import "./vendor/glightbox/css/glightbox.min.css"
 import * as bootstrap from "bootstrap"
