3eab4aeb2cb020a253df9e4ac4c640d2b741ce48
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-01-03 12:58:37 +0500

    fix: UI fixes and user logout fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,5 @@
 class DashboardController < ApplicationController
-	def index; end
+	def index
+		@layer_names = ['conv2d', 'average_pooling2d', 'conv2d_1', 'average_pooling2d_1', 'flatten', 'dense', 'dense_1', 'dense_2']
+	end
 end
