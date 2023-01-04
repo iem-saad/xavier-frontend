d7a270fbea0ce898c5e022c8c0d74e143f09a279
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-01-04 19:16:28 +0500

    feat: added graphical analysis for separate features

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,12 @@ class DashboardController < ApplicationController
 	before_action :initialize_backend_service
 
 	def index; end
+	def mutation_analysis; end
+	def select_mutation_operator; end
+	def graphical_analysis; end
+	def model_graphical_analysis; end
+	def tabular_analysis; end
+	def model_tabular_analysis; end
 
 	def mutation_testing
 		@model_names = @backend_serice.get_model_names()
@@ -13,40 +19,47 @@ class DashboardController < ApplicationController
 		@layer_names = @backend_serice.get_layer_names(params[:model_name])
 	end
 
-	def mutation_analysis; end
-	def select_mutation_operator; end
-	def graphical_analysis; end
-	def model_graphical_analysis
-		@org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
-		@mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
+	def selected_graphical_analysis
+		if selected_analysis[:specificity].present?
+			@org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
+			@mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
+		end
 
-		@org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
-		@mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
+		if selected_analysis[:class_accuracy].present?
+			@org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
+			@mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
+		end
 
-		@org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
-		@mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
+		if selected_analysis[:f1_score].present?
+			@org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
+			@mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
+		end
 
+		if selected_analysis[:recall].present?
+			@org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
+			@mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
+		end
 
-		@org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
-		@mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
+		if selected_analysis[:precision].present?
+			@org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
+			@mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
+		end
 
-		@org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
-		@mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
+		if selected_analysis[:sensitivity].present?
+			@org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
+			@mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
+		end
 
-		@org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
-		@mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
+		if selected_analysis[:auc].present?
+			@org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
+			@mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
+		end
 
-		@org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
-		@mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
-
-
-
-		@org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
-		@mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
+		if selected_analysis[:accuracy].present?
+			@org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
+			@mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
+		end
 	end
-
-	def tabular_analysis; end
-	def model_tabular_analysis; end
 
 	def get_layer_weights
 		@backend_serice.get_layer_weights(params[:model_name], params[:layer])
@@ -55,5 +68,9 @@ class DashboardController < ApplicationController
 	private
 		def initialize_backend_service
 			@backend_serice ||= ModelDetailsService.new
+		end
+
+		def selected_analysis
+			params.require(:analysis).permit(:specificity, :class_accuracy, :f1_score, :recall, :precision, :auc, :sensitivity, :accuracy)
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "dashbaord/mutation_analysis", to: "dashboard#mutation_analysis" 
   get "dashbaord/graphical_analysis", to: "dashboard#graphical_analysis" 
   get "dashbaord/graphical_analysis/:model_name", to: "dashboard#model_graphical_analysis", as: 'specific_model_graph_eval'
+  get "dashbaord/selected_graphical_analysis/:model_name", to: "dashboard#selected_graphical_analysis", as: 'selected_graphical_analysis'
   get "dashbaord/tabular_analysis", to: "dashboard#tabular_analysis" 
   get "dashbaord/tabular_analysis/:model_name", to: "dashboard#model_tabular_analysis" , as: 'specific_model_table_eval'
   get "dashbaord/get_model_details/:model_name/:operator", to: "dashboard#get_model_details", as: 'dashbaord_get_model_details' 
