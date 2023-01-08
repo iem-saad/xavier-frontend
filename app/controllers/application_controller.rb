b49ffeb30b30756f083e34c587fe8112544305d3
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-01-08 15:19:22 +0500

    feat: protect dashboard for invalid access and added after sign in path for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 	before_action :check_for_invalid_model_or_operator
 
+	def after_sign_in_path_for(resource)
+	  dashboard_index_path
+	end
+
 	private def check_for_invalid_model_or_operator
 		return redirect_to dashboard_index_path, alert: "Invalid Model Selected!" if params[:model_name].present? && !Rails.application.config.available_models.include?(params[:model_name])
 		return redirect_to dashboard_index_path, alert: "Invalid Operator Selected!" if params[:operator].present? && !Rails.application.config.available_operators.include?(params[:operator])
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,141 +1,146 @@
 class DashboardController < ApplicationController
-	before_action :initialize_backend_service
+  before_action :initialize_backend_service
+  before_action :protect_invalid_access
 
-	def index; end
-	def mutation_analysis; end
-	def select_mutation_operator; end
-	def graphical_analysis; end
-	def model_graphical_analysis; end
-	def tabular_analysis; end
-	def model_tabular_analysis; end
+  def index; end
+  def mutation_analysis; end
+  def select_mutation_operator; end
+  def graphical_analysis; end
+  def model_graphical_analysis; end
+  def tabular_analysis; end
+  def model_tabular_analysis; end
 
-	def mutation_testing
-		@model_names = @backend_serice.get_model_names()
-	end
+  def mutation_testing
+    @model_names = @backend_serice.get_model_names()
+  end
 
-	def get_model_details
-		return redirect_to dashbaord_mutation_testing_path, alert: "Invalid Model Selected!" unless params[:model_name].present?
+  def get_model_details
+    return redirect_to dashbaord_mutation_testing_path, alert: "Invalid Model Selected!" unless params[:model_name].present?
 
-		@layer_names = @backend_serice.get_layer_names(params[:model_name])
-		@model_image = @backend_serice.get_model_img(params[:model_name])
-	end
+    @layer_names = @backend_serice.get_layer_names(params[:model_name])
+    @model_image = @backend_serice.get_model_img(params[:model_name])
+  end
 
-	def selected_graphical_analysis
-		return redirect_to specific_model_graph_eval_path(model_name: params[:model_name]), alert: "Please Select Atleast One Method!" unless params[:analysis].present?
+  def selected_graphical_analysis
+    return redirect_to specific_model_graph_eval_path(model_name: params[:model_name]), alert: "Please Select Atleast One Method!" unless params[:analysis].present?
 
-		if selected_analysis[:specificity].present?
-			@org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
-			@mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
-		end
+    if selected_analysis[:specificity].present?
+      @org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
+      @mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
+    end
 
-		if selected_analysis[:class_accuracy].present?
-			@org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
-			@mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
-		end
+    if selected_analysis[:class_accuracy].present?
+      @org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
+      @mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
+    end
 
-		if selected_analysis[:f1_score].present?
-			@org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
-			@mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
-		end
+    if selected_analysis[:f1_score].present?
+      @org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
+      @mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
+    end
 
-		if selected_analysis[:recall].present?
-			@org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
-			@mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
-		end
+    if selected_analysis[:recall].present?
+      @org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
+      @mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
+    end
 
-		if selected_analysis[:precision].present?
-			@org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
-			@mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
-		end
+    if selected_analysis[:precision].present?
+      @org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
+      @mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
+    end
 
-		if selected_analysis[:sensitivity].present?
-			@org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
-			@mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
-		end
+    if selected_analysis[:sensitivity].present?
+      @org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
+      @mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
+    end
 
-		if selected_analysis[:auc].present?
-			@org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
-			@mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
-		end
+    if selected_analysis[:auc].present?
+      @org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
+      @mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
+    end
 
-		if selected_analysis[:accuracy].present?
-			@org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
-			@mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
-		end
-	end
+    if selected_analysis[:accuracy].present?
+      @org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
+      @mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
+    end
+  end
 
-	def selected_tabular_analysis
-		return redirect_to specific_model_table_eval_path(model_name: params[:model_name]), alert: "Please Select Atleast One Method!" unless params[:analysis].present?
+  def selected_tabular_analysis
+    return redirect_to specific_model_table_eval_path(model_name: params[:model_name]), alert: "Please Select Atleast One Method!" unless params[:analysis].present?
 
-		if selected_analysis[:specificity].present?
-			@org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
-			@mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
-		end
+    if selected_analysis[:specificity].present?
+      @org_specificity = @backend_serice.get_model_metric(params[:model_name], 'specificity').values.map(&:to_f)
+      @mutated_specificity = @backend_serice.get_model_metric("Mutant", 'specificity').values.map(&:to_f)
+    end
 
-		if selected_analysis[:class_accuracy].present?
-			@org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
-			@mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
-		end
+    if selected_analysis[:class_accuracy].present?
+      @org_class_accuracy = @backend_serice.get_model_metric(params[:model_name], 'class-accuracy').values.map(&:to_f)
+      @mutated_class_accuracy = @backend_serice.get_model_metric("Mutant", 'class-accuracy').values.map(&:to_f)
+    end
 
-		if selected_analysis[:f1_score].present?
-			@org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
-			@mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
-		end
+    if selected_analysis[:f1_score].present?
+      @org_f1_scores = @backend_serice.get_model_metric(params[:model_name], 'f1-score').values.map(&:to_f)
+      @mutated_f1_scores = @backend_serice.get_model_metric("Mutant", 'f1-score').values.map(&:to_f)
+    end
 
-		if selected_analysis[:recall].present?
-			@org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
-			@mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
-		end
+    if selected_analysis[:recall].present?
+      @org_recalls = @backend_serice.get_model_metric(params[:model_name], 'recall').values.map(&:to_f)
+      @mutated_recalls = @backend_serice.get_model_metric("Mutant", 'recall').values.map(&:to_f)
+    end
 
-		if selected_analysis[:precision].present?
-			@org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
-			@mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
-		end
+    if selected_analysis[:precision].present?
+      @org_precision = @backend_serice.get_model_metric(params[:model_name], 'precision').values.map(&:to_f)
+      @mutated_precision = @backend_serice.get_model_metric("Mutant", 'precision').values.map(&:to_f)
+    end
 
-		if selected_analysis[:sensitivity].present?
-			@org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
-			@mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
-		end
+    if selected_analysis[:sensitivity].present?
+      @org_sensitivity = @backend_serice.get_model_metric(params[:model_name], 'sensitivity').values.map(&:to_f)
+      @mutated_sensitivity = @backend_serice.get_model_metric("Mutant", 'sensitivity').values.map(&:to_f)
+    end
 
-		if selected_analysis[:auc].present?
-			@org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
-			@mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
-		end
+    if selected_analysis[:auc].present?
+      @org_auc = @backend_serice.get_model_metric(params[:model_name], 'auc').values.map(&:to_f)
+      @mutated_auc = @backend_serice.get_model_metric("Mutant", 'auc').values.map(&:to_f)
+    end
 
-		if selected_analysis[:accuracy].present?
-			@org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
-			@mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
-		end
-	end
+    if selected_analysis[:accuracy].present?
+      @org_accuracy = @backend_serice.get_model_metric(params[:model_name], 'model-accuracy').values.map(&:to_f)
+      @mutated_accuracy = @backend_serice.get_model_metric("Mutant", 'model-accuracy').values.map(&:to_f)
+    end
+  end
 
-	def get_layer_weights
-		@kernels = @backend_serice.get_layer_weights(params[:model_name], params[:layer])
-		@operator_list = @backend_serice.get_operator_names(1)
-	end
+  def get_layer_weights
+    @kernels = @backend_serice.get_layer_weights(params[:model_name], params[:layer])
+    @operator_list = @backend_serice.get_operator_names(1)
+  end
 
-	def compare_layer_weights
-		@org_kernels = @backend_serice.get_layer_weights(params[:model_name], params[:layer])
-		@mutated_kernels = @backend_serice.get_layer_weights("Mutant", params[:layer])
-	end
+  def compare_layer_weights
+    @org_kernels = @backend_serice.get_layer_weights(params[:model_name], params[:layer])
+    @mutated_kernels = @backend_serice.get_layer_weights("Mutant", params[:layer])
+  end
 
-	def put_layer_weights
-		return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), alert: "Error! Operator Value Not Selected!" if operator_params[:operator].eql?("change-neuron") && !operator_params[:op_value].present?
-		return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), alert: "Error! Invalid Operator Selected!" if !operator_params[:modal_row].present? || !operator_params[:modal_col].present? || !operator_params[:modal_kernel].present?
+  def put_layer_weights
+    return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), alert: "Error! Operator Value Not Selected!" if operator_params[:operator].eql?("change-neuron") && !operator_params[:op_value].present?
+    return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), alert: "Error! Invalid Operator Selected!" if !operator_params[:modal_row].present? || !operator_params[:modal_col].present? || !operator_params[:modal_kernel].present?
 
-		response = @backend_serice.generate_mutant(params[:model_name], params[:layer], operator_params[:operator], operator_params[:op_value], operator_params[:modal_row], operator_params[:modal_col], operator_params[:modal_kernel])
-		return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), notice: response.values[0]
-	end
+    response = @backend_serice.generate_mutant(params[:model_name], params[:layer], operator_params[:operator], operator_params[:op_value], operator_params[:modal_row], operator_params[:modal_col], operator_params[:modal_kernel])
+    return redirect_to dashbaord_get_layer_weights_path(model_name: params[:model_name], operator: params[:operator], layer: params[:layer]), notice: response.values[0]
+  end
 
-	private
-		def initialize_backend_service
-			@backend_serice ||= XavierBackendService.new
-		end
+  private
+    def initialize_backend_service
+      @backend_serice ||= XavierBackendService.new
+    end
 
-		def selected_analysis
-			params.require(:analysis).permit(:specificity, :class_accuracy, :f1_score, :recall, :precision, :auc, :sensitivity, :accuracy)
-		end
+    def selected_analysis
+      params.require(:analysis).permit(:specificity, :class_accuracy, :f1_score, :recall, :precision, :auc, :sensitivity, :accuracy)
+    end
 
-		def operator_params
-			params.require(:operator_type).permit(:modal_kernel, :modal_row, :modal_col, :operator, :op_value)
-		end
+    def operator_params
+      params.require(:operator_type).permit(:modal_kernel, :modal_row, :modal_col, :operator, :op_value)
+    end
+
+    def protect_invalid_access
+      return redirect_to new_user_session_path, alert: "Please Sign in to continue" unless current_user.present?
+    end
 end
