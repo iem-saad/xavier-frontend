01a99c334b78304f07de1e8c8067df5ccec1d5eb
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-03-04 20:38:57 +0500

    fix: UI beautification and added results to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_140508) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_153712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_140508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.jsonb "results"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
