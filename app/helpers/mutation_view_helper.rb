907b058d2a5d176a40c459102f6d86c39e628ff4
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-03-05 22:27:46 +0500

    feat: updated UI for project show page

--- app/helpers/mutation_view_helper.rb
+++ app/helpers/mutation_view_helper.rb
@@ -12,4 +12,12 @@ module MutationViewHelper
   def current_active_tab(url)
     return "active" if url == request.url
   end
+
+  def operator_conf(project)
+    if project.hyper_params.dig("layer").include?("conv")
+      "You have applied #{project.hyper_params.dig("operator_params")["operator"]} operator on #{project.hyper_params.dig("layer")} at kernel #{project.hyper_params.dig("operator_params")["modal_kernel"]}, row #{project.hyper_params.dig("operator_params")["modal_row"]} and column #{project.hyper_params.dig("operator_params")["modal_col"]}."
+    else
+      "You have applied #{project.hyper_params.dig("operator_params")["operator"]} operator on #{project.hyper_params.dig("layer")} at previous edge #{project.hyper_params.dig("operator_params")["modal_prev"]}, current edge #{project.hyper_params.dig("operator_params")["modal_curr"]}."
+    end
+  end
 end
