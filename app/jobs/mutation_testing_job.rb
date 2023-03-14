944c19fdd9c905ed9a44f5ae4fbfd38c7c66b03a
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-03-15 02:21:31 +0500

    fix: added project link to mail and fixed Backend JOB

--- app/jobs/mutation_testing_job.rb
+++ app/jobs/mutation_testing_job.rb
@@ -5,7 +5,7 @@ class MutationTestingJob < ApplicationJob
     @backend_serice ||= XavierBackendService.new
     response = {}
     response = @backend_serice.start_mutation_testing(project.id) if !project.completed?
-    if response["detail"].present? && response["detail"].include?("Success")
+    if response["message"].present? && response["message"].include?("Success")
       Notification.create(project: project, user: project.user, n_type: 0, text: "Successfully Processed")
       BackendMailer.notify_project_status(project).deliver_now
     end
--- app/mailers/backend_mailer.rb
+++ app/mailers/backend_mailer.rb
@@ -1,6 +1,7 @@
 class BackendMailer < ApplicationMailer
   def notify_project_status(project)
     @project = project
+    @link = "#{ view_context.link_to 'View Project', project_url(project) }"
     mail(to: project.user.email,
     subject: "Progress Status For Your Project. #{project.name}",
     from: "xavier.cfd@gmail.com")
