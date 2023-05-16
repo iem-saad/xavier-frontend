0df310330896ab2e0e8e05b4ad7dba624a03eb37
Saad Abdullah <f190108@cfd.nu.edu.pk>
2023-05-17 02:58:47 +0500

    fix: bug fix in project status mailer.

--- app/mailers/backend_mailer.rb
+++ app/mailers/backend_mailer.rb
@@ -1,9 +1,8 @@
 class BackendMailer < ApplicationMailer
   helper MutationViewHelper
 
-  def notify_project_status(project, pdf)
+  def notify_project_status(project)
     @project = project
-    attachments['result_report.pdf'] = pdf
     @link = "#{ view_context.link_to 'View Project', project_url(project) }"
     mail(to: project.user.email,
     subject: "Progress Status For Your Project. #{project.name}",
